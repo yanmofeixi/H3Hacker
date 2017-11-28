18f95a3ef716e91b6479a5f269189d32eff5c593
yanmofeixi <yanmofeixi@gmail.com>
2017-11-28 02:15:30 -0800

    Add support for 64bit

--- H3Hacker/Memory/ProcessMemoryReaderApi.cs
+++ H3Hacker/Memory/ProcessMemoryReaderApi.cs
@@ -7,15 +7,16 @@ namespace H3Hacker.Memory
 {
     public class ProcessMemoryReaderApi
     {
+        [StructLayout(LayoutKind.Sequential)]
         public struct MEMORY_BASIC_INFORMATION
         {
-            public int BaseAddress;
-            public int AllocationBase;
-            public int AllocationProtect;
-            public int RegionSize;
-            public int State;
-            public int Protect;
-            public int lType;
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public uint AllocationProtect;
+            public IntPtr RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
         }
 
         public struct SYSTEM_INFO
